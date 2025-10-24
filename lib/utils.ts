f3c78fb5ddf4ced221741f78357e769c89f65041
xavier8cartier <kpetrov1@tlu.ee>
2025-10-24 15:01:31 +0300

    feat(lib/supabase): export supabase and export fetchCards

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// This check can be removed, it is just for tutorial purposes
 export const hasEnvVars =
-  process.env.NEXT_PUBLIC_SUPABASE_URL &&
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+  !!process.env.NEXT_PUBLIC_SUPABASE_URL &&
+  !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export async function fetchCards() {
+  const res = await fetch("/api/cards");
+  return res.json();
+}
