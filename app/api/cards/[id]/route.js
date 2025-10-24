2d18f023dc803208ec66267b9a08c072daab7b28
xavier8cartier <kpetrov1@tlu.ee>
2025-10-24 15:07:19 +0300

    fix(components, ui, api): delete unused text from template

--- app/api/cards/[id]/route.js
+++ app/api/cards/[id]/route.js
@@ -1,7 +1,7 @@
 import { supabase } from "../../../../lib/supabase/client";
 
 export async function GET(_, { params }) {
-  const { id } = params;
+  const { id } = await params;
   const { data, error } = await supabase
     .from("cards")
     .select("*")
@@ -15,7 +15,7 @@ export async function GET(_, { params }) {
 }
 
 export async function PUT(request, { params }) {
-  const { id } = params;
+  const { id } = await params;
   const { question, answer } = await request.json();
   const { data, error } = await supabase
     .from("cards")
@@ -31,7 +31,7 @@ export async function PUT(request, { params }) {
 }
 
 export async function DELETE(_, { params }) {
-  const { id } = params;
+  const { id } = await params;
   const { error } = await supabase.from("cards").delete().eq("id", id);
   if (error)
     return new Response(JSON.stringify({ error: error.message }), {
